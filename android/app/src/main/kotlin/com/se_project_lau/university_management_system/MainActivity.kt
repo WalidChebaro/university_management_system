75ceb0362966b384f29f5c43219f8711a364303f
WalidChebaro <55446702+WalidChebaro@users.noreply.github.com>
2020-05-04 15:15:54 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/se_project_lau/university_management_system/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.se_project_lau.university_management_system
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
